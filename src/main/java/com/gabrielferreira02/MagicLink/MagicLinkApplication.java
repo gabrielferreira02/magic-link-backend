1c4b1b693824085e8a9a66d494bc15b0d020ecd5
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-03-06 17:37:47 -0300

    Upload project

---
+++ src/main/java/com/gabrielferreira02/MagicLink/MagicLinkApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira02.MagicLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicLinkApplication.class, args);
+	}
+
+}
